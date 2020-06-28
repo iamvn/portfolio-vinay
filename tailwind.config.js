8be0c4e76dfe9e97b57df9412e199ac56f632859
iamvn <38998836+iamvn@users.noreply.github.com>
2020-06-28 19:57:07 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  purge: [],
+  theme: {
+    extend: {},
+  },
+  variants: {},
+  plugins: [],
+}
+
+module.exports = {
+  variants: {
+  overflow: ['responsive', 'hover', 'focus'],
+  }
+}
